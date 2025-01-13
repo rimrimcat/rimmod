5463497e746925a45b3e0e05d6b7aa2951b67ed4
RimRim <rimrimrimrim1987@gmail.com>
2025-01-13 13:00:47 +0800

    edited some stuff

--- src/main/java/net/rimrim/rimmod/Config.java
+++ src/main/java/net/rimrim/rimmod/Config.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.rimrim.rimmod;
 
 import java.util.List;
 import java.util.Set;
@@ -13,7 +13,7 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Neo's config APIs
-@EventBusSubscriber(modid = ExampleMod.MODID, bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = RimMod.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class Config
 {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
